e7305834e792f664f6b3e3731eea0bd6260b62d9
CannaShaq <itscoldinwinter2@gmail.com>
2020-08-19 23:54:17 -0700

    forms
    forms almost completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,81 @@
-import React from 'react'
+import React from 'react';
+import * as yup from 'yup';
+import { useState, useEffect } from 'react';
+
+const formSchema = yup.object().shape({
+    email: yup.string().email("Must include valid email").required("Email is required for sign up."),
+    password: yup.string().required("Must include password")
+})
 
 const Login = () => {
 
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [errors, setErrors] = useState({
+        name:"",
+        email: "",
+        password: ""
+    })
+
+    const inputChange = e => {
+        e.persist();
+        const newFormData = {
+            ...formData,
+            [e.target.name]:
+                 e.target.value
+        };
+        
+        validateChange(e);
+        setFormData(newFormData);
+    }
+
+    const validateChange = e => {
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: ""
+                })
+            })
+            .catch(err => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: err.errors[0]
+                })
+            })
+    }
+
+    const formSubmit = () => {
+
+    }
+
 
     return(
-        <>
-        <h1>Login</h1>
-        </>
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={formSubmit}>
+                <label htmlFor="email">
+                    Email: 
+                    <br/>
+                    <input type="email" name="email" onChange={inputChange} value={formData.email}/>
+                </label>
+                <br/>
+                <label htmlFor="password">
+                    Password: 
+                    <br/>
+                    <input type="password" name="email" onChange={inputChange} value={formData.password}/>
+                    <br/>
+                </label>
+                <button type="submit" disabled={buttonDisabled}>Submit</button>
+            </form>
+        </div>
     )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { useState, useEffect} from 'react';
 
 const Register = () => {
 
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
 
     return(
-        <>
-        <h1>Register</h1>
-        </>
+        <div>
+            <h1>Register</h1>
+            <label htmlFor="name">
+                    Name: 
+                    <br/>
+                    <input type="text"/>
+                    <br/>
+                </label>
+                <label htmlFor="email">
+                    Email: 
+                    <br/>
+                    <input type="email"/>
+                    <br/>
+                </label>
+                <label htmlFor="password">
+                    Password: 
+                    <br/>
+                    <input type="password"/>
+                    <br/>
+                </label>
+                <br/>
+                <button type="submit" disabled={buttonDisabled}>Submit</button>
+        </div>
     )
 }
 
