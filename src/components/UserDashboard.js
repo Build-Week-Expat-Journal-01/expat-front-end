2f659d388252f622ce58a00d3a74be4109da2de6
dustin <dustnclaytiner@gmail.com>
2020-08-20 21:45:43 -0500

    login and Register modified to comminicate with API

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import AxiosWithAuth from '../utils/AxiosWithAuth'
+import axios from 'axios'
 
 const UserDashboard = () => {
+
+    axios.post('https://build-week-expat-journal-1.herokuapp.com/')
+
     return(
         <>
         </>
