696d95955a2076baff1266889b57ff38f408abb8
CannaShaq <itscoldinwinter2@gmail.com>
2020-08-26 11:27:50 -0700

    FIXED Login button started Edit story
    FIXED Login button, Started EditStory,  started styling userDashboard,

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -2,6 +2,7 @@ import React,{useEffect} from 'react'
 import AxiosWithAuth from '../utils/AxiosWithAuth'
 import axios from 'axios'
 import UserStoryCard from './UserStoryCard'
+import {Container, Row, Col, Button} from 'reactstrap';
 import {useHistory} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {fetchData} from '../actions/index'
@@ -24,13 +25,22 @@ const UserDashboard = (props) => {
     }
 
     return(
-        <>
-        <button onClick={logout}>Logout</button>
+        <Container>
+            <Row>
+                {/*extra Col tags for positioning*/}
+                <Col></Col>
+                <Col></Col>
+                <Col></Col>
+                <Col sm="12" lg="2">
+                    <Button color="danger" onClick={logout}>Logout</Button>
+                </Col>
+            </Row>
+        
         <h1>User DAAAAAAASH</h1>
-        <button onClick={addStory}>Add Story</button>
+        <Button color="info" onClick={addStory}>Add Story</Button>
         <UserStoryCard/>
         
-        </>
+        </Container>
     )
 }
 
